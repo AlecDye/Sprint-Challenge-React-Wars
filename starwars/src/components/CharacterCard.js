86aebbe63b44cc58413b66daf1ff02bdd6e8f9a1
Alec Dye <alecdye@gmail.com>
2020-01-24 08:41:31 -0800

    rendering API state

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -1,10 +1,17 @@
 import React from "react";
 
+//* name, birth_year, homeworld, species
 //TODO: structure card based on props
-const CharacterCard = () => {
+const CharacterCard = (props) => {
     return (
-        <>
-        </>
+        <div>
+            <h2>{props.name}</h2>
+            <div>
+                <p>{props.birth_year}</p>
+                <p>{props.homeworld}</p>
+                <p>{props.species}</p>
+            </div>
+        </div>
     );
 };
 
