dbc23a5f5f18f423789e05ab3e4a11028e529865
Alec Dye <alecdye@gmail.com>
2020-01-24 08:38:56 -0800

    receiving API state

--- starwars/src/components/CardList.js
+++ starwars/src/components/CardList.js
@@ -2,26 +2,40 @@ import React, { useState, useEffect } from "react";
 import CharacterCard from "./CharacterCard";
 import axios from "axios";
 
-//! API url: "https://swapi.co/api/people"
+//* API url: "https://swapi.co/api/people/"
+//* data>resultsArray>characterObjects>
+//* name, birth_year, homeworld, species
 
 const CardList = () => {
-    const [] = useState([]);
+    const [state, setState] = useState([]);
     useEffect(() => {
         axios
-            .get("https://swapi.co/api/people")
+            .get("https://swapi.co/api/people/")
             .then(response => {
-                console.log(response)
+                // console.log(response)
+                setState(response.data.results)
+                // console.log(response.data.results)
             })
             .catch(error => {
                 console.log("Data not returning", error)
             })
     }, []);
     return (
-        <>
-            <CharacterCard />
-        </>
+        <div>
+            {state.map(character => {
+                return (
+                    <CharacterCard
+                        name={character.name}
+                        dob={character.birth_year}
+                        home={character.homeworld}
+                        species={character.species}
+                    />
+                )
+            })}
+        </div>
     );
 };
 
+//! error: CharacterCard's children need unique "key" prop
 
 export default CardList;
