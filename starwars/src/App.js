19f39116b74e01bb8e6cde7b50d194ff82f9b46b
Alec Dye <alecdye@gmail.com>
2020-01-24 08:27:27 -0800

    formatting quotes

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import CardList from "./components/CardList";
+import "./App.css";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
